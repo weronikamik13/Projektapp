d42f0e2307a66ad2b75fbc4a21eb0141147ad597
weronikamik13 <50332965+weronikamik13@users.noreply.github.com>
2019-07-04 23:08:21 +0200

    Add files via upload

---
+++ RunFlask.py
@@ -0,0 +1,6 @@
+import os
+from app import app
+port = int(os.environ.get("PORT", 5000))
+app.run(debug = True)
+print "starting flask server hostname:% port:%" % (host, port)
+app.run(host="0.0.0.0", port=port)
