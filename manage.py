2628be6f6d344f60c521985d6171496c7a983298
weronikamik13 <50332965+weronikamik13@users.noreply.github.com>
2019-07-10 17:31:25 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+#from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell
 
 from setup import app, db
+
+app = app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
 
 migrate = Migrate(app, db)
 
